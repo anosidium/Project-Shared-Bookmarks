42516610a2bac873f603e32fc9bc2eb68065a670
Mansoor Munawar <mansoor.munawar448@gmail.com>
2026-02-12 22:33:57 +0000

    Refactor code to support unit testing by exporting functions from the main script file

--- example.test.js
+++ example.test.js
@@ -1,6 +1,7 @@
 import assert from "node:assert";
 import test from "node:test";
 import { getUserIds } from "./storage.js";
+import { sortBookmarksByDate, incrementLikeCount } from "./script.js";
 
 test("User count is correct", () => {
   assert.equal(getUserIds().length, 5);
